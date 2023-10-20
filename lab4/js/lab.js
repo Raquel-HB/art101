5fa457c32f806f59e09ea03055f6e304aa8fce6f
Raquel-HB <rherrer9@ucsc.edu>
2023-10-19 23:26:35 -0700

    lab

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -2,13 +2,8 @@
 // Author: Raquel And Juilana
 // Date: Oct 19, 2023
 
-// Print the board before each turn
-    // Loop through each row
-        // Loop through each space
-            // Print X, O, or Space
-            // Print a | between
-        // Print a newline at the end
-        // Print a horizontal line between
+
+
 // Constants
 
 // Functions
@@ -26,3 +21,10 @@ function main() {
 
 // let's get this party started
 main();
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
