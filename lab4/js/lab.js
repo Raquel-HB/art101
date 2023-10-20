0c8b5331cfb2d71804570c9539a62a1b071f59f6
Raquel-HB <rherrer9@ucsc.edu>
2023-10-19 21:23:15 -0700

    info

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,14 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Raquel And Juilana
+// Date: Oct 19, 2023
 
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
 // Constants
 
 // Functions
---
+++ lab4/js/lab4.js
@@ -0,0 +1,21 @@
+
+// Rock, Paper, Scissors
+// An implementation of the game
+// Raquel Herrera rherrer9@ucsc.edu>
+// October 2023
+
+// User input (Rock, Paper, Scissors)
+    // Computer chooses moves 
+
+
+        // Countdown
+
+         // Winner vs Loser 
+            // Rock beats Scissors
+            //Scissors beats Paper
+             //Paper beats Rock
+
+
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
