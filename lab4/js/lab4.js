dcec4bc1bd4c4cb21316a0debf1bd6211c18374d
Raquel-HB <rherrer9@ucsc.edu>
2023-10-22 18:39:13 -0700

    work

--- lab4/js/lab4.js
+++ lab4/js/lab4.js
@@ -4,18 +4,32 @@
 // Raquel Herrera rherrer9@ucsc.edu>
 // October 2023
 
-// User input (Rock, Paper, Scissors)
-    // Computer chooses moves 
+Userinput (Rock, Paper, Scissors)
+
+//Computer chooses moves
+     //Random
+//Countdown
+    //1, 2, 3
+
+//Winner vs Loser 
+    //Win 
+       //Rock beats Scissors
+      //Scissors beats Paper
+     //Paper beats Rock
+
+ //Lose
+     //  Scissors vs paper
+     // paper vs rocks
+    
+// no points 
+     // if both players pick the same things
+
+                 //Add up  points
+                //Best of 3 games
+                    //End after 3 games
+                //Output winner signs with lighting animation efffect
 
 
-        // Countdown
+        // script.js
 
-         // Winner vs Loser 
-            // Rock beats Scissors
-            //Scissors beats Paper
-             //Paper beats Rock
-
-
-            // Print a | between
-        // Print a newline at the end
-        // Print a horizontal line between
+  
