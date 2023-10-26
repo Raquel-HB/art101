f9b9cb1e03970163c89071b02bb7230675088d1a
Raquel-HB <rherrer9@ucsc.edu>
2023-10-26 08:11:49 -0700

    cars

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/* 
+ * Author: Raquel Herrera-Bernardino <rherrer9@ucsc.edu>
+ * Created: October 24 2023
+ * License: Public Domain
+ */
+
+
+ // Define Variables 
+make = "Volkswagen";
+model = "Volkswagen New Bettle";
+color = "red";
+year = "2008";
+
+// calculate 
+age = 2023 - year; 
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>"); 
+document.writeln("Year: " + year + "<br>"); 
+document.writeln("Age: " + age + "years<br>" ); 
